4fbecb7b4e25bca932ec1e722f1514ccd56f9594
stacy <stacydn23@gmail.com>
2020-10-30 18:04:23 -0400

    commit tree 39cb143515cca905a1e83f03dc58bc3401d28e35

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+       
+        names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+  def roll_call_dwarves(names)
+       names.each_with_index {|name, index| puts " #{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planter_calls)
+  planter_calls.collect {|planter| planter.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+      calls.any? do |call|
+    call.length > 4
+      end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food.find do |food|
+    cheese_types.include?(food)
+    end
 end
